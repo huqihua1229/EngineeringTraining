3f3f14f3aace31daec5ad09312cde4d4f000d99a
“sistine” <sistinefibel29@outlook.com>
2023-03-19 20:43:46 +0800

    2023-03-19 1

--- src/main/java/com/hqh/controller/LoginController.java
+++ src/main/java/com/hqh/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.hqh.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.hqh.dto.JsonMessage;
+import com.hqh.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,14 @@ public class LoginController {
 
     @RequestMapping("login")
     public String login(String account, String password){
+
+        ApplicationContext context =new ClassPathXmlApplicationContext("spring/applicationContext.xml");
+        UserService userService = (UserService) context.getBean("userService");
+
+        JsonMessage jsonMessage = userService.login(account, password);
+
+        String result = JSONObject.toJSONString(jsonMessage);
+        System.out.println(result);
         return "account: " + account + " password: " + password;
     }
 }
